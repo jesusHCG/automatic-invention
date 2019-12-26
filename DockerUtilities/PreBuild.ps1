75fb45d681f11bcb78a37b90780eb03abd53c990
t-hernjes <jesus.hc@globant.com>
2019-12-26 12:03:04 -0600

    Update PreBuild.ps1

--- DockerUtilities/PreBuild.ps1
+++ DockerUtilities/PreBuild.ps1
@@ -1,7 +1,7 @@
 " -=-=- PrebuilsScript < $env:ENVIRONMENT_LANGUAGE > JenkinsFile -=-=- "
 
-Write-Host $env:ENV_NUGET_API_KEY 
+Write-Host "Secret text: $env:ENV_NUGET_API_KEY"
 
+Write-Host "Secret user: $env:ENV_NUGET_USERNAME"
 
-
-Write-Host $env:ENV_NUGET_USERNAME
+Write-Host "Secret password: $env:ENV_NUGET_PASSWORD"
