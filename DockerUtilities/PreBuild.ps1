7636749dbb414213fa4c774ac2b7b9a74f39afdd
t-hernjes <jesus.hc@globant.com>
2019-12-26 12:00:03 -0600

    Update PreBuild.ps1

--- DockerUtilities/PreBuild.ps1
+++ DockerUtilities/PreBuild.ps1
@@ -1,9 +1,7 @@
 " -=-=- PrebuilsScript < $env:ENVIRONMENT_LANGUAGE > JenkinsFile -=-=- "
 
-if($ENV_NUGET_API_KEY -eq "millavedenuget"){
-    Write-Host ">>> Secret text correcto"
-}
+Write-Host $env:ENV_NUGET_API_KEY 
 
-if($ENV_NUGET_USERNAME -eq "NUGET_CREDENTIAL"){
-    Write-Host ">>> Username correcto"
-}
+
+
+Write-Host $env:ENV_NUGET_USERNAME
