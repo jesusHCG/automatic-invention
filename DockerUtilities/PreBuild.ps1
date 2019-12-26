fa03a604d4caaedb5f45d2df267ec8591b41468a
t-hernjes <jesus.hc@globant.com>
2019-12-26 13:09:10 -0600

    Update PreBuild.ps1

--- DockerUtilities/PreBuild.ps1
+++ DockerUtilities/PreBuild.ps1
@@ -5,3 +5,8 @@ Write-Host "Secret text: $env:ENV_NUGET_API_KEY"
 Write-Host "Secret user: $env:ENV_NUGET_USERNAME"
 
 Write-Host "Secret password: $env:ENV_NUGET_PASSWORD"
+
+Write-Host "ADS_USER_NAME: $env:ADS_USER_PASSWORD"
+
+Write-Host "ADS_USER_PASSWORD: $env:ADS_USER_PASSWORD"
+
