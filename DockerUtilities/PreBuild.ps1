56f3d0364b1cf4f51c23fe6071ffc3e028a1293a
t-hernjes <jesus.hc@globant.com>
2020-01-27 15:50:31 -0600

    Update PreBuild.ps1

--- DockerUtilities/PreBuild.ps1
+++ DockerUtilities/PreBuild.ps1
@@ -12,5 +12,4 @@ Write-Host "ADS_USER_PASSWORD: $env:ADS_USER_PASSWORD"
 
 Write-Host "LANGUAGE_CHOICES: $env:LANGUAGE_CHOICES"
 
-Invoke-Item "$env:WORKSPACE\DockerUtilities\executePS.ps1" -a $env:LANGUAGE_CHOICES
-
+Invoke-Expression "$env:WORKSPACE\DockerUtilities\executePS.ps1 -a $env:LANGUAGE_CHOICES"
