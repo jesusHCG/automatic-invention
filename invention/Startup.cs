dd91f8c51f9a3227ccf00ad9fa2f2e413eed674b
t-hernjes <jesus.hc@globant.com>
2020-03-15 22:30:34 -0600

    Update Startup.cs

--- invention/Startup.cs
+++ invention/Startup.cs
@@ -18,6 +18,7 @@ namespace invention
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            
         }
 
         public IConfiguration Configuration { get; }
